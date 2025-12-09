a1061885ece9eb0aadf124248e0afa689276ef40
jassem-manita <jasemmanita00@gmail.com>
2025-12-09 02:41:41 +0100

    Restructure Zelda theme and update Neovim configuration for automatic loading

--- lua/zelda/init.lua
+++ lua/zelda/init.lua
@@ -3,14 +3,14 @@ local M = {}
 function M.setup(opts)
     opts = opts or {}
     -- Add any theme-specific setup logic here
-    vim.cmd("highlight Normal guibg=#0F1419 guifg=#A8C5D8")
-    vim.cmd("highlight Comment guifg=#6B5A73")
-    vim.cmd("highlight Constant guifg=#E6B450")
-    vim.cmd("highlight Identifier guifg=#3A7CA8")
-    vim.cmd("highlight Statement guifg=#E06B75")
-    vim.cmd("highlight PreProc guifg=#98C379")
-    vim.cmd("highlight Type guifg=#5A9BB8")
-    vim.cmd("highlight Special guifg=#5A9BB8")
+    vim.cmd("highlight Normal guibg=#0D1117 guifg=#C9D1D9")
+    vim.cmd("highlight Comment guifg=#6A737D")
+    vim.cmd("highlight Constant guifg=#F0A45D")
+    vim.cmd("highlight Identifier guifg=#58A6FF")
+    vim.cmd("highlight Statement guifg=#FF7B72")
+    vim.cmd("highlight PreProc guifg=#A5D6A7")
+    vim.cmd("highlight Type guifg=#79C0FF")
+    vim.cmd("highlight Special guifg=#79C0FF")
 end
 
 return M
